2006a0d42988b2e5cb1a8f13d2cc21a433fd5b44
impso-arion <lifesizemail@gmail.com>
2021-07-01 08:28:06 +0000

    チャプター2済時点（チェックボックスまで)

--- hello/forms.py
+++ hello/forms.py
@@ -4,4 +4,4 @@ class HelloForm(forms.Form):
     name = forms.CharField(label='name')
     mail = forms.CharField(label='mail')
     age = forms.IntegerField(label='age')
-    
+    check = forms.BooleanField(label='Checkbox', required=False)
--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HelloView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(r'', HelloView.as_view(), name='index'),
     
     ]
