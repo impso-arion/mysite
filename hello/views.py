02a8d179bb411888dd2e46f36bb8ca00eec97a64
impso-arion <lifesizemail@gmail.com>
2021-07-01 01:42:03 +0000

    page89

--- hello/views.py
+++ hello/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'hello/index.html')
+    params = {
+        'title':'hello/Index',
+        'msg':'これは、サンプルで作ったページです。',
+        'goto':'next',
+    }
     
+    return render(request, 'hello/index.html', params)
+    
+def next(request):
+    params = {
+        'title':'hello/Next',
+        'msg':'これは、もう一つのページです。',
+        'goto':'index',
+    }
+    
+    return render(request, 'hello/index.html', params)
