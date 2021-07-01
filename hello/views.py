6dcacebf09152ba039a4afc2e4c8915e050d5628
impso-arion <lifesizemail@gmail.com>
2021-07-01 06:54:40 +0000

    page103

--- hello/views.py
+++ hello/views.py
@@ -1,43 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-#92ページ
+from . forms import HelloForm
+#98ページ
 def index(request):
     params = {
-        'title':'hello/Index',
-        'msg':'お名前は?',
+        'title':'Hello',
+        'message':'Your data',
+        'form':HelloForm()
     }
+    if (request.method == 'POST'):
+        params['message'] = '名前:' + request.POST['name'] + \
+            '<br>メール:' + request.POST['mail'] + \
+            '<br>年齢:' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'hello/index.html', params)
-
-def form(request):
-    msg = request.POST['msg']
-    params = {
-        'title':'Hello/Form',
-        'msg':'こんにちは' + msg + 'さん',
-        
-    }
-    return render(request, 'hello/index.html', params)
-
-
-
-
-
-'''
-def index(request):
-    params = {
-        'title':'hello/Index',
-        'msg':'これは、サンプルで作ったページです。',
-        'goto':'next',
-    }
-    
-    return render(request, 'hello/index.html', params)
-    
-def next(request):
-    params = {
-        'title':'hello/Next',
-        'msg':'これは、もう一つのページです。',
-        'goto':'index',
-    }
-    
-    return render(request, 'hello/index.html', params)
-'''
